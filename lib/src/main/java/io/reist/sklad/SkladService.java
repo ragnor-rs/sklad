8d024b6181e35851ddf3552019c80ec16d49af19
Renat Sarymsakov <ragnor.rs@gmail.com>
2016-06-24 12:35:38 +0300

    second commit

---
+++ lib/src/main/java/io/reist/sklad/SkladService.java
@@ -0,0 +1,4 @@
+package io.reist.sklad;
+
+public interface SkladService {
+}
---
+++ lib/src/test/java/io/reist/sklad/SkladServiceTest.java
@@ -0,0 +1,17 @@
+package io.reist.sklad;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Reist on 24.06.16.
+ */
+public class SkladServiceTest {
+
+    @Test
+    public void testDummy() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
