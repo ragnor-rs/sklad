552d0bb70d7e859a6b085b77a4c48616368ce937
Renat Sarymsakov <ragnor.rs@gmail.com>
2018-02-21 18:02:10 +0300

    minor fix

--- lib/src/main/java/io/reist/sklad/FileBasedCachedStorageStates.java
+++ lib/src/main/java/io/reist/sklad/FileBasedCachedStorageStates.java
@@ -1,5 +1,7 @@
 package io.reist.sklad;
 
+import android.support.annotation.NonNull;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -9,37 +11,46 @@ import java.io.IOException;
 
 public class FileBasedCachedStorageStates implements CachedStorageStates {
 
+    @NonNull
     private final File root;
 
-    public FileBasedCachedStorageStates(String root) {
+    public FileBasedCachedStorageStates(@NonNull String root) {
         this.root = new File(root);
     }
 
-    public FileBasedCachedStorageStates(File root) {
+    public FileBasedCachedStorageStates(@NonNull File root) {
         this.root = root;
     }
 
     @Override
     public void setFullyCached(Storage local, String id, boolean fullyCached) throws IOException {
-        File marker = new File(root, id);
+
+        if (!root.mkdirs() && !root.exists()) {
+            throw new IOException("Cache state directory doesn't exist");
+        }
+
+        File marker = getMarkerFile(id);
+
         if (fullyCached) {
-            try {
-                if (!marker.createNewFile()) {
-                    throw new IOException("Cannot create marker file for " + id);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (!marker.createNewFile()) {
+                throw new IOException("Cannot create marker file for " + id);
             }
         } else {
             if (!marker.delete()) {
                 throw new IOException("Error deleting marker file for " + id);
             }
         }
+
+    }
+
+    @NonNull
+    private File getMarkerFile(String id) {
+        return new File(root, id);
     }
 
     @Override
     public boolean isFullyCached(Storage local, String id) {
-        return (new File(root, id)).exists();
+        return getMarkerFile(id).exists();
     }
 
 }
